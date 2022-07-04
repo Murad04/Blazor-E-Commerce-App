c5de02cf5512a9d589057a148ec524d4b7f45537
Murad <67361462+Murad04@users.noreply.github.com>
2022-07-04 20:01:24 +0400

    Solved problems at 619b9002de9cdc52f7ec81dc7b4d86bb6603078c

---
+++ Plugins/Blazor-E-Commerce.StateStore.DI/StateStoreBase.cs
@@ -0,0 +1,18 @@
+﻿using Blazor_E_Commerce.UseCases.PluginInterfaces.StateStore;
+
+namespace Blazor_E_Commerce.StateStore.DI
+{
+    public class StateStoreBase : IStateStore
+    {
+        protected Action listeners;
+
+        public void AddStateChangeListeners(Action listener) => this.listeners += listener;
+
+        public void BroadCastStateChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveStateChangeListeners(Action listener) => this.listeners += listener;
+    }
+}

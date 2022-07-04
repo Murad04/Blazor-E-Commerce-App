b21957ef6e37e7b7392b3888e7e1d873f1a39f2a
Murad <67361462+Murad04@users.noreply.github.com>
2022-07-04 15:32:59 +0400

    Added Order Line Item Component and interface

---
+++ Blazor-E-Commerce.UseCases/ShoppingCardScreen/ViewShoppingCardUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Blazor_E_Commerce.CoreBusiness.Models;
+using Blazor_E_Commerce.UseCases.PluginInterfaces.UI;
+using Blazor_E_Commerce.UseCases.ShoppingCardScreen.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_E_Commerce.UseCases.ShoppingCardScreen
+{
+    public class ViewShoppingCardUseCase : IViewShoppingCardUseCase
+    {
+        private readonly IShoppingCard shoppingCard;
+
+        public ViewShoppingCardUseCase(IShoppingCard shoppingCard)
+        {
+            this.shoppingCard = shoppingCard;
+        }
+
+        public Task<Order> Execute()
+        {
+            return shoppingCard.GetOrderAsync();
+        }
+    }
+}

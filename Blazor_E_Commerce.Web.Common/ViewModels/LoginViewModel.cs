b15268bfa419b069806178cc9791db75bf3b6d04
Murad <67361462+Murad04@users.noreply.github.com>
2022-07-08 15:51:31 +0400

    Added login page and simple changes

---
+++ Blazor_E_Commerce.Web.Common/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_E_Commerce.Web.Common.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}

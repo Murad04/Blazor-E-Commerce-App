2072ed4b64a350d97270cba95861394cf4e3f4ea
Murad <67361462+Murad04@users.noreply.github.com>
2022-11-09 09:46:52 +0400

    Fixed .NET Information Disclosure Vulnerability #1 amd .NET Information Disclosure Vulnerability #2

--- DemoDapper/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dapper;
-using System.Data;
-using System.Data.SqlClient;
-
-var connectStr = "Data Source=DESKTOP-8UQUVJ2;Initial Catalog=Blazor_E_Commmerce_App;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-using (IDbConnection connection=new SqlConnection(connectStr))
-{
-    var results = connection.Query("SELECT *FROM Product");
-}
